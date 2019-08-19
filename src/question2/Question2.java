c98c26a2e7015628c8276cd57924efa0e925725f
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 14:42:34 +0530

    add new file:Question2.java

---
+++ src/question2/Question2.java
@@ -0,0 +1,13 @@
+package question2;
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s1 = input.nextLine();
+        String s2 = input.nextLine();
+        System.out.printf("%s University %s ", s1, s2);
+    }
+
+}
