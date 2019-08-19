e6b5bd4273a19b944ba606439258267a228cc950
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 15:27:40 +0530

    removing error in Question6.java

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Question6 {
     public static void main(String[] args) {
-        Scanner input new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
     }
 }
