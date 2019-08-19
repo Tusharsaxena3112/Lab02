cc50311293f17ac38a01b82aee33db012a937ccb
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 14:28:58 +0530

    add input for variable:(name , String)

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-//        String name=input.nextLine();
+        String name = input.nextLine().trim(); //method chaining
 //        System.out.printf("Welcome , %s",name);
         input.close();
     }
