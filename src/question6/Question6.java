cfc0bda2b85297eb31e7c55e2227b055faf5870f
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 15:31:43 +0530

    adding output

--- src/question6/Question6.java
+++ src/question6/Question6.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Question6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        char ch1 = input.nextLine().trim().charAt(0);
+        char ch2 = input.nextLine().trim().charAt(0);
+        if (ch1 > ch2) {
+            System.out.println(ch2 + " " + ch1);
+        } else {
+            System.out.println(ch1 + " " + ch2);
+        }
+        input.close();
     }
 }
