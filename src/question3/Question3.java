073411505a952356a286ab8ada8ad896088d047c
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 14:51:11 +0530

    add new file Question3.java

---
+++ src/question3/Question3.java
@@ -0,0 +1,17 @@
+package question3;
+
+import java.util.Scanner;
+
+public class Question3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.printf("%d + %d = %d\n", num1, num2, num1 + num2);
+        System.out.printf("%d - %d = %d\n", num1, num2, num1 - num2);
+        System.out.printf("%d * %d = %d\n", num1, num2, num1 * num2);
+        System.out.printf("%d / %d = %d\n", num1, num2, num1 / num2);
+        System.out.printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+    }
+
+}
