7bb740a5bf3ef9271fb01e9cfecdc1ebeaac736e
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 14:47:51 +0530

    modifying Scanner

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String first = input.nextLine();
-        String second = input.nextLine();
+        String first = input.nextLine().trim();
+        String second = input.nextLine().trim();
         System.out.printf("%s University %s ", first, second);
         input.close();
 
