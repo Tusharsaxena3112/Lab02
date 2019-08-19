61467e4aa891aa623b72ba89655db7418f04c6a1
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-19 15:07:18 +0530

    add file Question4.java

---
+++ src/question4/Question4.java
@@ -0,0 +1,18 @@
+package question4;
+
+import java.util.Scanner;
+
+public class Question4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        if (num1 > 0) {
+            System.out.println("Positive");
+        } else if (num1 < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+}
