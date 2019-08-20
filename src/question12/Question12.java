0bcbcb2dcda02ed7ebd2b6272ed7d85731263a90
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-20 21:48:04 +0530

    adding output

--- src/question12/Question12.java
+++ src/question12/Question12.java
@@ -1,7 +1,19 @@
 package question12;
 
+import java.util.Scanner;
+
 public class Question12 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int rem;
+        int sum = 0;
+        int num = input.nextInt();
+        for (int i = num; i > 0; i = i / 10) {
+            rem = i % 10;
+            sum += rem;
+        }
+        System.out.println(sum);
+        input.close();
     }
+
 }
