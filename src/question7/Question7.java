e3484fd42b318e1feadd9e51018be3fb6f6b26bb
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-20 21:34:08 +0530

    adding output

--- src/question7/Question7.java
+++ src/question7/Question7.java
@@ -1,5 +1,17 @@
 package question7;
 
-public class Question7 {
+import java.util.Scanner;
 
+public class Question7 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char c1 = input.next().charAt(0);
+        if ((int) c1 >= 65 && (int) c1 <= 91 || (int) c1 >= 97 && (int) c1 <= 122) {
+            System.out.println("Alphabet");
+        } else if ((int) c1 >= 48 && (int) c1 <= 57) {
+            System.out.println("Digit");
+        } else {
+            System.out.println("special character");
+        }
+    }
 }
