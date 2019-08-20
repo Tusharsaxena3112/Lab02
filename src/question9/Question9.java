bc4810bd06eab5d58342f0a72e85a31b194046f0
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-20 21:39:43 +0530

    adding output

--- src/question9/Question9.java
+++ src/question9/Question9.java
@@ -1,7 +1,16 @@
 package question9;
 
+import java.util.Scanner;
+
 public class Question9 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        char c = input.next().charAt(0);
+        if ((int) c >= 65 && (int) c <= 90) {
+            System.out.printf("%c", (int) c + 32);
+        } else if ((int) c >= 97 && (int) c <= 122) {
+            System.out.printf("%c", (int) c - 32);
+        }
+        input.close();
     }
 }
