8faf7fbca32b36802fa5aa1b4694e2849f5ab322
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-20 21:44:44 +0530

    adding output

--- src/question11/Question11.java
+++ src/question11/Question11.java
@@ -1,7 +1,21 @@
 package question11;
 
+import java.util.Scanner;
+
 public class Question11 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int start = input.nextInt();
+        int end = input.nextInt();
+        int m;
+        if (start % 2 != 0) {
+            m = start + 1;
+        } else {
+            m = start;
+        }
+        for (int i = m; i <= end; i = i + 2) {
+            System.out.println(i);
+        }
+        input.close();
     }
 }
