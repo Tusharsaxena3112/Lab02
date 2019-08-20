ec1ef739cb059e4f3f7e587e014d65135ef366f5
Tusharsaxena3112 <tushar.saxena_cs18@gla.ac.in>
2019-08-20 21:53:35 +0530

    adding output

--- src/question15/Question15.java
+++ src/question15/Question15.java
@@ -1,7 +1,22 @@
 package question15;
 
+import java.util.Scanner;
+
 public class Question15 {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int num1 = num;
+        int rem = 0, rev = 0;
+        for (int i = num; i > 0; i = i / 10) {
+            rem = i % 10;
+            rev = rev * 10 + rem;
+        }
+        if (rev == num1) {
+            System.out.printf("%d is a palindrome", num1);
+        } else {
+            System.out.printf("%d is not a palindrome", num1);
+        }
+        input.close();
     }
 }
